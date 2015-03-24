2d8189dcda33dae166e0eed68703e66d275d39a5
Darren <darrenfrenkel@gmail.com>
2015-03-24 15:06:21 +1100

    first commit

---
+++ tournament/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Tournament(models.Model):
+    pass
+
+
+class Player(models.Model):
+    pass
+
+class Match(models.Model):
+    pass
